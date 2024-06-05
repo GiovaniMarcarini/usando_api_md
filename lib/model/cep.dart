b42b2ca577e55840fde2592d3c5e19caf053097c
giovani <inf_giovani@hotmail.com>
2024-06-04 21:21:48 -0300

    Service cep

--- lib/model/cep.dart
+++ lib/model/cep.dart
@@ -21,4 +21,7 @@ class Cep{
   Cep({this.cep, this.logradouro, this.complemento, this.bairro, this.localidade,
   this.uf, this.ibge, this.gia, this.codArea, this.siafi});
 
+  factory Cep.fromJson(Map<String, dynamic> json) => _$CepFromJson(json);
+  Map<String, dynamic> toJson() => _$CepToJson(this);
+
 }
